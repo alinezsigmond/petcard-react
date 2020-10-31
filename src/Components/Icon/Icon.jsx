45aec5d155d4ecaf533abb2dfa3d9151993d65ce
alinezsigmond <alinezsigmond@hotmail.com>
2020-10-31 16:58:44 -0300

    css do header

--- src/Components/Icon/Icon.jsx
+++ src/Components/Icon/Icon.jsx
@@ -5,11 +5,11 @@ import './icon.css'
 
 export default function Icon(props) {
     return (
-        <div>
-            <div className="icon">
+        <>
+            <div className="icon" >
                 <img src={ props.img } alt="" />
             </div>
             <p className="icon-title"> {props.text} </p>
-        </div>
+        </>
     )
 }
