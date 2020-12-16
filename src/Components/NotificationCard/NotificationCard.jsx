c473f49671c20de0758e6405ffc1627501b1dd43
alinezsigmond <alinezsigmond@hotmail.com>
2020-12-16 00:43:12 -0300

    pagina de notificações +/- concluida

--- src/Components/NotificationCard/NotificationCard.jsx
+++ src/Components/NotificationCard/NotificationCard.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Card } from './style'
+import Switch from '@material-ui/core/Switch'
 
 export default function NotificationCard(props) {
-
+    const [state, setState] = React.useState({
+        checkedA: true,
+        checkedB: true,
+    });
+    
+      const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
     return (
         <Card>
-            <p className='med'> {props.remedio} </p>
+            <p className='med'> {props.titulo} </p>
+            <div className='switch' >
+                <Switch
+                    checked={state.checkedB}
+                    onChange={handleChange}
+                    color="primary"
+                    name="checkedB"
+                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                    />
+                </div>
             <p className='day'> {props.dia} </p>
-            <p className='time'> {props.horario} </p>
+            <p className='time'> {props.hora} </p>
         </Card>
     )
 }
--- src/Components/NotificationCard/style.js
+++ src/Components/NotificationCard/style.js
@@ -2,10 +2,29 @@ import styled from 'styled-components'
 
 export const Card = styled.div `
     background-color: #FFF;
-    width: 90%;
+    width: 85%;
     height: 90px;
+    margin-left: auto;
+    margin-right: auto;
     border-radius: 6px;
     color: #3C6382;
     font-family: Arial, Helvetica, sans-serif;
     font-size: 14px;
+    margin-bottom: 5%;
+    .med {
+        margin: 5%;
+        font-size: 15px;
+    }
+    .switch {
+        margin-left: 78%;
+        margin-top: -14%;
+    }
+    .day {
+        margin-left: 4%;
+    }
+    .time {
+        text-align: right;
+        margin-right: 7%;
+        margin-top: -9%;
+    }
 `
